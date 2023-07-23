59eab2379cd7b9be106f3501d339f33066b66825
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-07-23 13:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// script.js
 function getFormvalue() {
-    //Write your code here
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
 
+  // Display the first and last name using alert
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
+
+// Attach the getFormvalue function to the form submission
+document.getElementById('userForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  getFormvalue();
+});
